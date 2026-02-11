743300bde22bc9f23f1532a23e96d9b51d738e27
cAllum <93334063+iamabigbrain@users.noreply.github.com>
2026-02-11 10:36:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,22 @@ document.addEventListener("DOMContentLoaded", function () {
   // ================================
   // HERO IMAGE MOUSE PULL
   // ================================
-  const heroImage = document.querySelector(".hero-left img");
+  
+const heroImage = document.querySelector(".hero-left img");
 
-  if (heroImage) {
-    document.addEventListener("mousemove", e => {
-      const x = (window.innerWidth / 2 - e.clientX) / 30;
-      const y = (window.innerHeight / 2 - e.clientY) / 30;
+document.addEventListener("mousemove", e => {
 
-      heroImage.style.transform = `translate(${x}px, ${y}px)`;
-    });
-  }
+  const x = (e.clientX - window.innerWidth / 2) / 25;
+  const y = (e.clientY - window.innerHeight / 2) / 25;
+
+  gsap.to(heroImage, {
+    x: x,
+    y: y,
+    duration: 0.6,
+    ease: "power3.out"
+  });
+
+});
 
 
   // ================================
